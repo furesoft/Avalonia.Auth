01356be0bce400f869cabbb4214a367eb8f6af89
Chris Anders <filmee24@gmail.com>
2025-07-19 20:30:17 +0200

    Added WorldID Provider

---
+++ src/Providers/Avalonia.Auth.WorldID/WorldIdOptions.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Auth.OAuth;
+
+namespace Avalonia.Auth.WorldID;
+
+public class WorldIdOptions : OAuthOptions
+{
+    public WorldIdOptions()
+    {
+        Authority = "https://issuer.hello.coop/";
+        ClientId = "app_7VOU7y3FRsPuO2ALng6YQtYc_sWG";
+        Scope = "openid profile name picture github email";
+        ClientSecret = "ZWm-UprQfZ-kRl_TIUaQa";
+    }
+}
