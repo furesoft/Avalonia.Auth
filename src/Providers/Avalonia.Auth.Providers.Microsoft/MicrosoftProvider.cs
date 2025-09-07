474652b0902992d63d0b9854da3e3286b18182e4
Chris Anders <filmee24@gmail.com>
2025-09-07 10:58:10 +0200

    feat: Add Microsoft provider
    fixes #8

---
+++ src/Providers/Avalonia.Auth.Providers.Microsoft/MicrosoftProvider.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Auth.OAuth;
+using Avalonia.Media;
+
+namespace Avalonia.Auth.Providers.Microsoft;
+
+public class MicrosoftProvider : OAuthProvider<MicrosoftOptions>
+{
+    public override string Label => "Login with Microsoft";
+    public override Color Background => Color.Parse("#24292e");
+    public override Color Foreground => Color.Parse("#ffffff");
+    public override IImage? Icon { get; } = GetIcon("avares://Avalonia.Auth.Providers.Microsoft/Assets/microsoft.png");
+}
