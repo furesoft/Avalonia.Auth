988be74e52e3d088322f9c852c5c58c3e3df6db1
Chris Anders <filmee24@gmail.com>
2026-04-17 15:07:04 +0200

    feat: Improve browser verification by adding support for different application lifetimes

--- src/Avalonia.Auth/BrowserVerification.cs
+++ src/Avalonia.Auth/BrowserVerification.cs
@@ -8,6 +8,19 @@ namespace Avalonia.Auth;
 
 internal class BrowserVerification
 {
+    private TopLevel GetTopLevel()
+    {
+        if (Application.Current!.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            return  desktop.MainWindow!;
+        }
+        else if (Application.Current.ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+        {
+            return TopLevel.GetTopLevel(singleView.MainView)!;
+        }
+
+        throw new InvalidOperationException("Unsupported application lifetime");
+    }
     public async Task<bool> Open(OAuthOptions options)
     {
         var redirectUri = options.UseHostUriRedirectUrl ? "http://localhost:7890/" : "http://127.0.0.1:7890/";
@@ -18,14 +31,12 @@ internal class BrowserVerification
         var client = new OidcClient(options);
         var state = await client.PrepareLoginAsync();
 
-        var lifetime = Application.Current!.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+        var topLevelView = GetTopLevel();
         var authOptions = new WebAuthenticatorOptions(
             RequestUri: new Uri(state.StartUrl),
             RedirectUri: new Uri(redirectUri));
 
-        var rawUrl = await WebAuthenticationBroker.AuthenticateAsync(
-            lifetime!.MainWindow!,
-            authOptions);
+        var rawUrl = await WebAuthenticationBroker.AuthenticateAsync(topLevelView, authOptions);
 
         var result = await client.ProcessResponseAsync(rawUrl.CallbackUri.ToString(), state);
 
