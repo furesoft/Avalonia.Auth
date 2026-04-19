4048becfeccb678f52e421ec9ea2effbc6516ce6
Chris Anders <filmee24@gmail.com>
2026-04-19 13:12:43 +0200

    feat: Add .env file for OAuth provider configurations and remove hardcoded secrets

--- src/Providers/Avalonia.Auth.Provider.Google/GoogleOptions.cs
+++ src/Providers/Avalonia.Auth.Provider.Google/GoogleOptions.cs
@@ -8,7 +8,5 @@ public class GoogleOptions : OAuthOptions
     {
         Authority = "https://accounts.google.com/";
         Scope = "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email";
-        ClientId = "148559676177-0mm94sppm60kqv6veht8pg29vnh6ji0s.apps.googleusercontent.com";
-        ClientSecret = "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";
     }
 }
--- src/Providers/Avalonia.Auth.Provider.Twitch/TwitchOptions.cs
+++ src/Providers/Avalonia.Auth.Provider.Twitch/TwitchOptions.cs
@@ -7,7 +7,6 @@ public class TwitchOptions : OAuthOptions
     public TwitchOptions()
     {
         Authority = "https://id.twitch.tv/oauth2";
-        ClientId = "ns6y2ybvbtr0jmh56ns702sd9hjd6u";
         Scope = "openid";
         RedirectUri = "https://localhost/";
 
